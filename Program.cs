242c695adbdbe676ae2821abad234ea2595193c8
StevenSuv <89829762+StevenSuv@users.noreply.github.com>
2021-09-08 14:45:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat
+            //iga mängija viskab täringut (1-6)
+            //programm kuvab nende visete tulemused ja kuvab ka, kes on võitja
+            // - ehk kes viskas suurema
+
+            Random rnd = new Random();
+
+            string playerOne = "Hairy Pot";
+            string playerTwo = "Voltorb";
+
+            int HarryThrow = rnd.Next(1, 7);
+            int VoldThrow = rnd.Next(1, 7);
+
+            Console.WriteLine($"{playerOne} threw {HarryThrow};");
+            Console.WriteLine($"{playerTwo} threw {VoldThrow};");
+
+            if (HarryThrow > VoldThrow)
+            {
+                Console.WriteLine($"{playerOne} wins!");
+            }
+            else if (HarryThrow < VoldThrow)
+            {
+                Console.WriteLine($"{playerTwo} wins!");
+            }
+            else
+            {
+                Console.WriteLine("Draw! Let them try again.");
+            }
+        }
+    }
+}
